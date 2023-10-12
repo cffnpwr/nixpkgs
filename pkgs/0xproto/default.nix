af51e428401c50d4d9938f90c3c4c54d6d76546f
cffnpwr <cffnpwr@gmail.com>
2023-10-12 13:10:04 +0900

    feat: 0xProtoをnerdfontに

--- pkgs/0xproto/default.nix
+++ pkgs/0xproto/default.nix
@@ -1,17 +1,30 @@
-{ lib, stdenvNoCC, fetchzip }:
+{ lib, stdenvNoCC, fetchzip, pkgs }:
 
 let
   maintainers = import ../../maintainers.nix;
 in
 stdenvNoCC.mkDerivation rec {
   pname = "0xProto";
-  version = "1.001";
+  version = "1.300";
   versionNonPeriod = builtins.replaceStrings [ "." ] [ "_" ] version;
 
   src = fetchzip {
     url = "https://github.com/0xType/0xProto/releases/download/${version}/0xProto_${versionNonPeriod}.zip";
     sha256 = "0i7y2c2g72lcz00xl3d57gyn3ckx9ic8nn5bi7pjs20vmwj6mfwd";
   };
+
+  buildInputes = with pkgs; [
+    nerd-font-patcher
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    nerd-font-patcher 0xProto-Regular.otf
+    nerd-font-patcher 0xProto-Regular.ttf
+
+    runHook postBuild
+  '';
 
   installPhase = ''
     runHook preInstall
