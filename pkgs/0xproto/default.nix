21deb4a31b3fb33703214e2bca4058c628ff8cc5
cffnpwr <cffnpwr@gmail.com>
2023-10-12 13:32:47 +0900

    fix: add pkgs

--- pkgs/0xproto/default.nix
+++ pkgs/0xproto/default.nix
@@ -1,4 +1,4 @@
-{ lib, stdenvNoCC, fetchzip, nerd-font-patcher }:
+{ lib, stdenvNoCC, fetchzip, pkgs }:
 
 let
   maintainers = import ../../maintainers.nix;
@@ -13,7 +13,7 @@ stdenvNoCC.mkDerivation rec {
     sha256 = "0i7y2c2g72lcz00xl3d57gyn3ckx9ic8nn5bi7pjs20vmwj6mfwd";
   };
 
-  buildInputes = [ nerd-font-patcher ];
+  buildInputes = [ pkgs.nerd-font-patcher ];
 
   buildPhase = ''
     runHook preBuild
