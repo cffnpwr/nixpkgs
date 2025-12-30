363711b4fdbba65d26275b8c021dcb1cf284e539
CaffeinePower <cffnpwr@gmail.com>
2025-12-30 22:26:41 +0900

    perf: :zap: flake-utilsからflake-partsに移行

--- flake.nix
+++ flake.nix
@@ -3,18 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nix-unit = {
+      url = "github:nix-community/nix-unit";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+      };
+    };
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
-      flake-utils,
+      flake-parts,
+      nix-unit,
       ...
     }:
-    let
-      lib = nixpkgs.lib;
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        nix-unit.modules.flake.default
+      ];
 
       systems = [
         "x86_64-linux"
@@ -22,82 +32,112 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      forAllSystems = flake-utils.lib.eachSystem systems;
 
-      libExports = import ./lib { inherit lib; };
-      internalLib = libExports.internalLib;
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          lib,
+          ...
+        }:
+        let
+          libExports = import ./lib { inherit lib; };
+          internalLib = libExports.internalLib;
 
-      # Helper to wrap modules with internalLib in extraSpecialArgs
-      wrapModulesWithInternalLib = dir: {
-        _module.args.internalLib = internalLib;
-        imports = lib.collect builtins.isString (internalLib.modulePathsFromDir dir);
-      };
-    in
-    {
-      # Overlays
-      overlays.default =
-        final: prev:
-        import ./pkgs {
-          pkgs = final;
-        }
-        // {
-          lib = prev.lib.extend (
-            _: _: {
-              maintainers = (prev.lib.maintainers or { }) // internalLib.maintainers;
+          allPackages = import ./pkgs { inherit pkgs; };
+        in
+        {
+          # Configure pkgs with overlays
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+            config.allowUnfree = true;
+          };
+
+          # Legacy packages (all packages from ./pkgs)
+          legacyPackages = allPackages;
+
+          # Formatter
+          formatter = pkgs.nixfmt-rfc-style;
+
+          # Development shell
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              git
+              nil
+              nixd
+              nixfmt-rfc-style
+            ];
+          };
+
+          # Applications
+          apps = {
+            generate-github-actions-matrix = {
+              type = "app";
+              program = import ./scripts/generate-github-actions-matrix.nix {
+                inherit pkgs lib;
+                flake = self;
+              };
+            };
+
+            update-pkg = {
+              type = "app";
+              program = import ./scripts/update-pkg.nix {
+                inherit pkgs lib allPackages;
+              };
+            };
+          };
+
+          # nix-unit configuration
+          nix-unit = {
+            # Pass required inputs to nix-unit
+            inputs = {
+              inherit (inputs) nixpkgs flake-parts nix-unit;
+            };
+
+            # Collect all *.test.nix files
+            tests = internalLib.testsFromDir ./.;
+          };
+        };
+
+      flake =
+        let
+          lib = nixpkgs.lib;
+          libExports = import ./lib { inherit lib; };
+          internalLib = libExports.internalLib;
+
+          # Helper to wrap modules with internalLib in extraSpecialArgs
+          wrapModulesWithInternalLib = dir: {
+            _module.args.internalLib = internalLib;
+            imports = lib.collect builtins.isString (internalLib.modulePathsFromDir dir);
+          };
+        in
+        {
+          # Overlays
+          overlays.default =
+            final: prev:
+            import ./pkgs {
+              pkgs = final;
             }
-          );
-        };
-
-      # Home Manager modules
-      homeModules.default = wrapModulesWithInternalLib ./modules/home-manager;
-
-      # nix-darwin modules
-      darwinModules.default = wrapModulesWithInternalLib ./modules/darwin;
-
-      # NixOS modules
-      nixosModules.default = wrapModulesWithInternalLib ./modules/nixos;
-    }
-    // forAllSystems (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-          config.allowUnfree = true;
-        };
-
-        allPackages = import ./pkgs { inherit pkgs; };
-      in
-      {
-        legacyPackages = allPackages;
-
-        formatter = pkgs.nixfmt-rfc-style;
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            git
-            nil
-            nixd
-            nixfmt-rfc-style
-          ];
-        };
-
-        apps = {
-          generate-github-actions-matrix = {
-            type = "app";
-            program = import ./scripts/generate-github-actions-matrix.nix {
-              inherit pkgs lib;
-              flake = self;
+            // {
+              lib = prev.lib.extend (
+                _: _: {
+                  maintainers = (prev.lib.maintainers or { }) // internalLib.maintainers;
+                }
+              );
             };
-          };
 
-          update-pkg = {
-            type = "app";
-            program = import ./scripts/update-pkg.nix {
-              inherit pkgs lib allPackages;
-            };
-          };
+          # Home Manager modules
+          homeModules.default = wrapModulesWithInternalLib ./modules/home-manager;
+
+          # nix-darwin modules
+          darwinModules.default = wrapModulesWithInternalLib ./modules/darwin;
+
+          # NixOS modules
+          nixosModules.default = wrapModulesWithInternalLib ./modules/nixos;
         };
-      }
-    );
+    };
 }
